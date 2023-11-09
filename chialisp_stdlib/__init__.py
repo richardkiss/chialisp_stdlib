243441c4860657918e5d66dd67a6fcc3bfe49d2a
Richard Kiss <him@richardkiss.com>
2023-11-09 14:37:00 -0800

    py38

--- chialisp_stdlib/__init__.py
+++ chialisp_stdlib/__init__.py
@@ -1,4 +1,8 @@
-from importlib.resources import files
+try:
+    from importlib.resources import files
+except ImportError:
+    # for py3.8
+    from importlib_resources import files
 
 
 STABLE_INCLUDE_DIRECTORY = files(__package__) / "stable"
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
+PACKAGES = ["chialisp_stdlib", "chialisp_stdlib.nightly", "chialisp_stdlib.stable"]
+
 setup(
-    packages=["chialisp_stdlib"],
+    packages=PACKAGES,
     package_data={"": ["stable/*.clib", "nightly/*.clib"]},
 )
